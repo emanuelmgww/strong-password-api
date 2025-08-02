4816fa01f857ad884338f03b8c3d60d61f215e81
emanuelmgww <emanuelmirandagaldino40197@gmail.com>
2025-08-02 19:37:09 -0300

    feat: regenerate Swagger documentation after updating host and scheme

--- docs/docs.go
+++ docs/docs.go
@@ -44,9 +44,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
-	BasePath:         "/",
-	Schemes:          []string{},
+	Host:             "strong-password-api.onrender.com/",
+	BasePath:         "",
+	Schemes:          []string{"https"},
 	Title:            "API Gerador de Senha Forte",
 	Description:      "Um gerador de senha aleatória segura.",
 	InfoInstanceName: "swagger",
